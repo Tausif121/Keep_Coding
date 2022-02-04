de53bbe46617ef1a4a1dc2e9a0ddc2f80d89e185
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-04 11:11:01 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -10,22 +10,17 @@ class Solution{
     // n: size of array
     //Function to find the sum of contiguous subarray with maximum sum.
     long long maxSubarraySum(int arr[], int n){
-        
-        long long sum=INT_MIN;
-        long long temp=0;
-        for(int i=0; i<n; i++){
-            
-            for(int j=i; j<n; j++){
-                temp=temp+arr[j];
-                
-                if(temp>sum){
-                    sum=temp;
-                } 
-            }
-            temp=0;
-            
-        }
-        return sum;
+{
+   int max_so_far = arr[0];
+   int curr_max = arr[0];
+
+   for (int i = 1; i < n; i++)
+   {
+        curr_max = max(arr[i], curr_max+arr[i]);
+        max_so_far = max(max_so_far, curr_max);
+   }
+   return max_so_far;
+}
         
     }
 };
