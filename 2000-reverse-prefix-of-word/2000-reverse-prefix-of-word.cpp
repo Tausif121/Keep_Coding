2fb00412680fe42f28cd24239c24f8c15d254625
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-06 07:59:28 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (74.52%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
-    string reversePrefix(string word, char ch) {
-        reverse(begin(word), begin(word) + word.find(ch) + 1);
-        return word;
+    string reversePrefix(string &word, char ch) {
+        int i = word.find_first_of(ch);
+        string s, t;
+        if(!i) return word;
+        else{
+            s = word.substr(0, i+1);
+            t = word.substr(i+1);
+            
+            reverse(s.begin(), s.end());
+        }
+        return s+t;
     }
 };
