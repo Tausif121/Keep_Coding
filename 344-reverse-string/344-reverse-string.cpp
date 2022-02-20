2034a1d8c861b0cbc9a80296f7766f0dba5e4905
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-20 06:43:18 +0530

    Time: 27 ms (60.49%), Space: 23.2 MB (41.95%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       int i=0;
+        int j=s.size()-1;
+        char t;
+        while(i<j)
+        {
+          t=s[i];
+          s[i]=s[j];    
+           s[j]   =t;
+            i++;
+            j--;
+        }
+    }
+};
