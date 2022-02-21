9a72a8d8ad338c37a26d19b81403e8ea208d7aa0
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-21 07:32:02 +0530

    Time: 39 ms (29.69%), Space: 14.8 MB (22.77%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,44 @@
+/*
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int> mp;
+     for(int i=0;i<arr.size();i++)
+     {
+       mp[arr[i]]++;  
+     }
+       
+    
+        for(auto j:mp)
+        {
+            
+            if(j.second == 1)
+            {
+                k-=1;
+                if(k==0){
+                   return j.first;
+                   
+                }
+            }
+        }
+       
+        return "";
+    }
+};
+*/
+class Solution {
+public:
+string kthDistinct(vector<string>& arr, int k) {
+    unordered_map<string, int> m;
+    for (auto &i : arr)
+        m[i]++;
+    for (auto &j : arr){
+        if (m[j] == 1 )
+            k--;
+    if(k==0)
+            return j;
+    }
+    return "";
+}
+     
+};
