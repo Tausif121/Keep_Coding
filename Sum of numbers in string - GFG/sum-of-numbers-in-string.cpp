9e3604c4c92ef6b02f22a4023bc8f60b69c6fa26
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-23 11:58:16 +0530

    Added solution - LeetHub

---
+++ Sum of numbers in string - GFG/sum-of-numbers-in-string.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+// C++ program to calculate sum of all numbers present
+// in a string containing alphanumeric characters
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to calculate sum of all numbers present in a string.
+    int findSum(string str)
+    {
+    	int sum=0,num=0;
+    	for(int i=0;i<str.size();i++)
+    	{
+    	    if(str[i] >= '0' && str[i]<= '9')
+    	    num=num*10+(str[i]-'0');
+    	    else
+    	    {
+    	        sum+=num;
+    	        num=0;
+    	    }
+    	}
+    	return sum+num;
+    
+   }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	// input alphanumeric string
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin>>str;
+	    Solution obj;
+	    cout << obj.findSum(str);
+        cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
