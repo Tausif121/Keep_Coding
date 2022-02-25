56a251129cd498193e8b0e14214c33328bf22e4f
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-25 15:20:06 +0530

    Added solution - LeetHub

---
+++ Remove character - GFG/remove-character.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    string removeChars(string string1, string string2) {
+        // code here
+        string st="";
+        for(int i=0;i<string1.size();i++)
+        {
+            if(string2.find(string1[i]) == string::npos)
+            {
+                st+=string1[i];
+            }
+        }
+       return st; 
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string string1,string2;
+        cin >> string1; 
+        cin >> string2;
+        Solution ob;
+        cout << ob.removeChars(string1,string2) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
