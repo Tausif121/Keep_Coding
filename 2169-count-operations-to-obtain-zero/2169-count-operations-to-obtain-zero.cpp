4cb1a75bff96a023340cc9b2adb022234a196b73
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-28 07:09:43 +0530

    Time: 6 ms (19.14%), Space: 5.9 MB (74.61%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOperations(int n1, int n2) {
+    int c=0;
+    while(n1!=0 && n2!=0)
+    {
+      c+=1;
+      if(n1>n2)
+     {
+       n1-=n2;
+      }
+   else 
+    {
+     n2-=n1;
+    }
+   } 
+  return c;      
+   }
+};
