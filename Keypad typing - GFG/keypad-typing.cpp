9ab7d25c65b93e446ef4175124228f85066dafbc
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-28 09:13:10 +0530

    Added solution - LeetHub

--- Keypad typing - GFG/keypad-typing.cpp
+++ Keypad typing - GFG/keypad-typing.cpp
@@ -27,47 +27,15 @@ int main()
 
 
 //Function to find matching decimal representation of a string as on the keypad.
-string printNumber(string s, int n) 
-{
-    //code here
-    //s=to_lower(s);
-    string ans="";
-    for(int i=0;i<n;i++)
-    {
-      if(s[i]=='a' || s[i]=='b' || s[i]=='c')
-      ans+='2';
-    
-    
-     
-      else if(s[i]=='d' || s[i]=='e' || s[i]=='f')
-      ans+='3';
-    
-     
-      else if(s[i]=='g' || s[i]=='h' || s[i]=='i')
-      ans+='4';
-    
-     
-      else if(s[i]=='j' || s[i]=='k' || s[i]=='l')
-      ans+='5';
-    
-     
-      else if(s[i]=='m' || s[i]=='n' || s[i]=='o')
-      ans+='6';
-    
-     
-      if(s[i]=='p' || s[i]=='q' || s[i]=='r'|| s[i]=='s')
-      ans+='7';
-    
-     
-      if(s[i]=='t' || s[i]=='u' || s[i]=='v')
-      ans+='8';
-    
-     
-      else if(s[i]=='w' || s[i]=='x' || s[i]=='y'|| s[i]=='z')
-      ans+='9';
-    }
 
-    return ans;
-    
-    }
+string printNumber(string s, int n) {
+char arr[] = "22233344455566677778889999";
+string res;
 
+for(int i=0; i<n; i++){
+res += arr[s[i]- 'a'];
+}
+return res;
+  
+   
+}
