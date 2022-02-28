b19c0f1ae7fc50d29f16519a4717bae1e5eb5377
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-02-28 07:38:41 +0530

    Added solution - LeetHub

---
+++ Keypad typing - GFG/keypad-typing.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string printNumber(string , int);
+
+// User function pasted here
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s,temp;
+		cin>>s;
+		int i,len=s.length();
+		
+		cout << printNumber(s,len) << "\n";
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+//User function template for C++
+
+
+//Function to find matching decimal representation of a string as on the keypad.
+string printNumber(string s, int n) 
+{
+    //code here
+    //s=to_lower(s);
+    string ans="";
+    for(int i=0;i<n;i++)
+    {
+      if(s[i]=='a' || s[i]=='b' || s[i]=='c')
+      ans+='2';
+    
+    
+     
+      else if(s[i]=='d' || s[i]=='e' || s[i]=='f')
+      ans+='3';
+    
+     
+      else if(s[i]=='g' || s[i]=='h' || s[i]=='i')
+      ans+='4';
+    
+     
+      else if(s[i]=='j' || s[i]=='k' || s[i]=='l')
+      ans+='5';
+    
+     
+      else if(s[i]=='m' || s[i]=='n' || s[i]=='o')
+      ans+='6';
+    
+     
+      if(s[i]=='p' || s[i]=='q' || s[i]=='r'|| s[i]=='s')
+      ans+='7';
+    
+     
+      if(s[i]=='t' || s[i]=='u' || s[i]=='v')
+      ans+='8';
+    
+     
+      else if(s[i]=='w' || s[i]=='x' || s[i]=='y'|| s[i]=='z')
+      ans+='9';
+    }
+
+    return ans;
+    
+    }
+
