d0e73f2ba67fa05f18372cfcdef62b63e3c88ff0
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-05 07:28:50 +0530

    Added solution - LeetHub

--- Isomorphic Strings - GFG/isomorphic-strings.cpp
+++ Isomorphic Strings - GFG/isomorphic-strings.cpp
@@ -7,30 +7,39 @@ using namespace std;
 #define MAX_CHARS 256
 
  // } Driver Code Ends
+
 class Solution
 {
-    public:
-    //Function to check if two strings are isomorphic.
+   public:
+   //Function to check if two strings are isomorphic.
    bool areIsomorphic(string str1, string str2)
    {
-       
-       // Your code here
-       if(str1.length()!=str2.length())
-           return false;
-       int count1[26]={0};
-       int count2[26]={0};
-       for(int i=0; i<str1.length(); i++){
-           count1[str1[i]-'a']++;
-           count2[str2[i]-'a']++;
+
+       //Initialising arrays with length 26 as we have 26                           alphabets
+       int arr1[26]={0},arr2[26]={0};
+
+       //If the length of given 2 strings are not equal just                   return 0
+       if(str1.length() != str2.length()){
+           return 0;
        }
+
+       //Storing count of each char of string1 in an                             array(arr1)
        for(int i=0; i<str1.length(); i++){
-           if(count1[str1[i]-'a']!=count2[str2[i]-'a'])
-               return false;
+           arr1[str1[i] - 'a']++;
        }
-       return true;
+       for(int i=0; i<str2.length(); i++){
+           arr2[str2[i] - 'a']++;
+       }
+
+       //checking count of each char of str1 and str2 are                   equal or not in arr1 and arr2 respectively.If they are             not equal we can return 0 as they are not                               ISOMORPHIC(given in qstn).
+       for(int i=0; i<str1.length(); i++){
+           if(arr1[str1[i]-'a'] != arr2[str2[i]-'a']){
+               return 0;
+           }
+       }
+       return 1;
    }
 };
-
 // { Driver Code Starts.
 
 // Driver program
