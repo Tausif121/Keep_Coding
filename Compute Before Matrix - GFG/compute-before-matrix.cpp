212c2be83be58b0c4c376a804addbaf84c4cce72
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-06 14:35:49 +0530

    Added solution - LeetHub

---
+++ Compute Before Matrix - GFG/compute-before-matrix.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    vector<vector<int>> computeBeforeMatrix(int N, int M, vector<vector<int>> after){
+         int arr[N] = {0};
+        for(int i = 0; i < M; i++){
+            int curr = 0;
+            for(int j = 0; j < N; j++){
+                curr += arr[j];
+                after[j][i] -= curr;
+                arr[j] += after[j][i];
+                curr += after[j][i]; 
+            }
+        }
+        return after;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, M;
+        cin>>N>>M; 
+        vector<vector<int>> after(N,vector<int>(M));
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                cin>>after[i][j];
+            }
+        } 
+        Solution obj;
+        vector<vector<int>> before=obj.computeBeforeMatrix(N,M,after); 
+        for(int i=0;i<before.size();i++){
+            for(int j=0;j<before[i].size();j++){
+                cout<<before[i][j]<<' ';
+            } 
+            cout<<endl;
+        }
+    }
+}  // } Driver Code Ends
