482ccab16019f3ea0c7e663ffe7a177566aaf22f
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-12 11:33:11 +0530

    Time: 48 ms (92.53%), Space: 28.3 MB (6.26%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.cpp
+++ 561-array-partition-i/561-array-partition-i.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-      int n = nums.size();
         int s = 0;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i+=2)
+        for(int i=0;i<nums.size();i+=2)
         {s+=nums[i];
         }
         return s;
