8589867a889b237a70515014418a8664090a3104
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-19 08:10:45 +0530

    Time: 7 ms (5.03%), Space: 5.8 MB (65.52%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        if(num == 1)
-            return true;
-        long long lim=num/2;
-        for(long long i=0;i<=lim;i++)
-        {
-            if(i*i == num)
-                return true;
-        }
-        return false;
-    }
+       long long i;
+for( i=0; i*i<=num; i++)
+{
+if(i*i == num) return true;
+}
+return false;
+}
 };
