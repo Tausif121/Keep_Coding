cd5f485c1685a3491f2e36a6bbaf81e41fe2b120
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-25 15:01:26 +0530

    Added solution - LeetHub

--- Count the Zeros - GFG/count-the-zeros.cpp
+++ Count the Zeros - GFG/count-the-zeros.cpp
@@ -9,19 +9,39 @@ using namespace std;
 
 class Solution{   
 public:
-    int countZeroes(int arr[], int n) {
-        // code here
-        int c=0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(arr[i] == 0)
-            {
-                c+=1;
-            }
+    int firstZero(int arr[], int low, int high) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+
+            // Check if mid element is first 0
+            if ((mid == 0 || arr[mid - 1] == 1) && arr[mid] == 0) 
+                return mid;
+
+            // If mid element is not 0
+            if (arr[mid] == 1) 
+                return firstZero(arr, (mid + 1), high);
+
+            // If mid element is 0, but not first 0
+            else
+                return firstZero(arr, low, (mid - 1));
         }
-        return c;
+        return -1;
     }
+
+    int countZeroes(int arr[], int n) {
+        // Find index of first zero in given array
+        int first = firstZero(arr, 0, n - 1);
+
+        // If 0 is not present at all, return 0
+        if (first == -1) 
+            return 0;
+
+        return (n - first);
+    }
+
 };
+      
+      
 
 // { Driver Code Starts.
 int main() {
