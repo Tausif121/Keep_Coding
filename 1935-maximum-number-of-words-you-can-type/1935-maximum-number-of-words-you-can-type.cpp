e3378a887b91d4cfe9a04bce6073a311c36d61a8
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-26 09:13:37 +0530

    Time: 4 ms (65.70%), Space: 6.6 MB (66.75%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string bl) {
-   int ans=0;
-        bool okay=true;
-        for(int i=0;i<text.length();i++)
-        {
-            if(bl.find(text[i]) != -1)
-                okay=false;
-            if(text[i]==' ' || i ==text.length()-1)
-            {
-                if(okay==true)
-                    ans+=1;
-                else
-                    okay=true;
-            }
-        }
-        return ans;
+    bool isBadWord = 0; 
+	int res =0;
+	for (int i=0; i<text.length(); i++) 
+	{   for (int j=0; j<bl.length(); j++) 
+			if (text[i]==bl[j])  
+                isBadWord = 1;
+			if (text[i]==' ' || i==text.length()-1)
+		   { 
+            if (isBadWord==0) res++; 
+			else isBadWord=0; 
+		}
+	}
+	return res;    
     }
 };
