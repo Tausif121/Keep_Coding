bd6ab4514d3b781f3f71d966a9268da98b7f20ef
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-26 07:09:10 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (25.13%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-       
-        unordered_map<int,int> mp;
-        set<int> s;
-        for(int i=0;i<arr.size();i++)
-        {
-            mp[arr[i]]++;
-        }
-        for(auto i : mp)
-        {
-            if(s.find(i.second)!=s.end()){
-                return false;}
-            else
-            {
-                s.insert(i.second);
-            }
-            
-        }
-        return true;
+    
+        unordered_map<int,int>mp;
+        set<int>ans;
+      for(int i=0;i<arr.size();i++){
+        mp[arr[i]]++;
+      }
+      
+      for(auto i=mp.begin();i!=mp.end();i++){
+        ans.insert(i->second);
+      }
+      return ans.size()==mp.size() ?true:false;
+        
+      
     }
 };
