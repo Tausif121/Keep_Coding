4663a650c32b698992d21a099e856d41be02727f
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-27 15:40:43 +0530

    Added solution - LeetHub

--- Array Subset of another array - GFG/array-subset-of-another-array.cpp
+++ Array Subset of another array - GFG/array-subset-of-another-array.cpp
@@ -28,36 +28,17 @@ int main() {
 
 
 string isSubset(int a1[], int a2[], int n, int m) {
-    string yes="Yes";
-    string no="No";
-    unordered_set<int> s;
-    for(int i=0;i<n;i++)
-    {
-        s.insert(a1[i]);
-    }
-    
-    for(int i=0;i<m;i++)
-    {
-        if(s.find(a2[i]) == s.end())
-        return no;
-    }
-    return yes;
-    
-    
-    // sort(a1,a1+n);
-    // sort(a2,a2+m);
-    // int c=m;
-    // string y="Yes";
-    // string no="No";
-    // for(int i=0;i<m;i++)
-    // {
-    // for(int j=0;j<n;j++)
-    //   {
-    //     if(a2[i] == a1[j])
-    //     c-=1;
-    //   }
-    // }
-    // if(c==0)
-    // return y;
-    // return no;
+      unordered_map<int,int> mp ;
+   
+   for(int i=0;i<n; i++){
+       mp[a1[i]]++;
+   }
+   
+   for(int i=0; i<m; i++){
+       if(mp[a2[i]] != 1)
+          return "No" ;
+   }
+   return "Yes";
+   
+   
 }
