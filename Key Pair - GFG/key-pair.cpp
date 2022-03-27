1a7787d3e6e41b4d045a898f27c303b48984e711
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-27 19:55:48 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.cpp
+++ Key Pair - GFG/key-pair.cpp
@@ -11,17 +11,20 @@ public:
 	// Function to check if array has 2 elements
 	// whose sum is equal to the given value
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
-    unordered_set<int> s;
-    for(int i=0;i<n;i++)
-    {
-    if(s.find(x-arr[i])!=s.end())    
-    return true;
-    s.insert(arr[i]);
-    }
-    return false;
+	    unordered_map<int, int> hash;
+    	for (int i = 0; i < n; i++) {
+    		int numberToFind = x - arr[i];
+                //if numberToFind is found in map, return them
+    		if (hash.find(numberToFind) != hash.end()) {
+    		    return true;
+    		}
+                //number was not found. Put it in the map.
+    	//	hash[arr[i]] = i;
+    	hash[arr[i]]++;
+    	}
+    	return false;
 	}
 };
-
 // { Driver Code Starts.
 
 int main() {
