b8abf984a26277d33cb85e1803e7bd5b5ff93684
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-03-28 07:33:49 +0530

    Time: 4 ms (46.08%), Space: 6.1 MB (93.63%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -4,7 +4,7 @@ public:
         if(s.length() < 3) 
         return 0;
         int c=0;
-        for(int i=0;i<=s.size()-3;i++)
+        for(int i=0;i<s.size()-2;i++)
         {       
             if(s[i]!=s[i+1] && s[i+1]!=s[i+2] && s[i+2]!=s[i])
                 c+=1;
