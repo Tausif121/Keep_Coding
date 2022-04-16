1e3f239869a13d97f2e148dffff5d4616579ba4c
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-04-16 11:32:26 +0530

    Time: 20 ms (49.76%), Space: 5.7 MB (91.67%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+          if(x<0|| (x!=0 &&x%10==0))
+              return false;
       long long int n=x,rev=0;
         while(n>0)
         {
