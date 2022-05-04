ee2f64d15c9523c8b522649c3003e761e052728f
Md Tausif Siddiqui <64072708+Tausif121@users.noreply.github.com>
2022-05-04 12:40:47 +0530

    Time: 8 ms (18.35%), Space: 6.6 MB (88.22%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int tsum=0,ssum=0,d=0;
+        int tsum=0,ssum=0;
        for(int i=0;i<t.size();i++)
        {
            tsum+=t[i];
            ssum+=s[i];
        }
-     d=tsum-ssum;
-        return d;
+    
+        return tsum-ssum;
     }
 };
